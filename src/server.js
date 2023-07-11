6946b735a26f65f9232c16242e59436c30422c3e
Clarks1223 <pablo.uchuari@epn.edu.ec>
2023-07-10 22:54:15 -0500

    done

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@
 import express from 'express'
 import dotenv from 'dotenv'//manejor de variables de entorno
 import cors from 'cors';
-//imporatar las rutas
+//imporatar las rutas de veterinario
 import routerVeterinarios from './routers/veterinario_routes.js'
+//importar las rutas de paciente
+import routerPacientes from './routers/paciente_routes.js'
+
 
 // Inicializaciones
 const app = express()
@@ -22,6 +25,8 @@ app.use(express.json())//
 
 // Rutas 
 app.use('/api',routerVeterinarios)
+app.use('/api',routerPacientes)
+
 // Manejo de una ruta que no sea encontrada
 app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
 
